0dc31fea00d3403e752c067b6386f970da04d610
WellingtonKlinkowski <wellimgtomklin@gmail.com>
2023-12-05 09:04:20 -0300

    Alterações nas pastas e nomes para melhor organização

---
+++ Projeto Locadora Veiculos - Pessoal/locadora-veiculos/src/main/java/com/klinkowski/locadoraveiculos/teste.java
@@ -0,0 +1,13 @@
+package com.klinkowski.locadoraveiculos;
+
+public class teste {
+    public static void main(String[] args) {
+        while(true){
+
+        }
+
+        do {
+
+        } while(true);
+    }
+}
